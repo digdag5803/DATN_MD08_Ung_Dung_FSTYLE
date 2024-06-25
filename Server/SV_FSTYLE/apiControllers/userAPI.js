c20031f790112667465141dd6e2917695487f161
manhph28172 <manh20032301@gmail.com>
2024-06-25 11:25:39 +0700

    signIn_signUp

--- Server/SV_FSTYLE/apiControllers/userAPI.js
+++ Server/SV_FSTYLE/apiControllers/userAPI.js
@@ -25,6 +25,8 @@ exports.singUp = async (req, res) => {
     return res.status(200).json({
       message: "Đăng ký thành công.",
       result: objUser,
+
+      
     });
   } catch (error) {
     return res.status(500).json({
